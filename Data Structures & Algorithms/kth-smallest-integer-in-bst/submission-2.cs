e078910935c4f7167a7535afba71282880226554
Thiago Araujo <62454605+thiaraujo66@users.noreply.github.com>
2026-07-29 23:14:54 -0300

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        int n = 0;
+        Stack<TreeNode> stack = new();
+        TreeNode cur = root;
+
+        while(cur != null || stack.Count > 0)
+        {
+            while(cur != null)
+            {
+                stack.Push(cur);
+                cur = cur.left;
+            }
+
+            cur = stack.Pop();
+            n += 1;
+
+            if (n == k)
+                return cur.val;
+
+            cur = cur.right;
+        }
+        
+        return -1;
+    }
+}
