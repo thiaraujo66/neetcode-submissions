92678aff1671cb3d170cfa739ae1f5ae2a5bd815
Thiago Araujo <62454605+thiaraujo66@users.noreply.github.com>
2026-08-02 20:45:09 -0300

    Add: palindrome-number - submission-3

---
+++ Data Structures & Algorithms/palindrome-number/submission-3.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int div = 1;
+        while (x / div >= 10)
+        {
+            div *= 10;
+        }
+
+        while (x > 0)
+        {
+            if ((x/div) != x % 10)
+                return false;
+
+            x = (x % div)/10;
+            div = (div/100);
+        }
+
+        return true;
+    }
+}
